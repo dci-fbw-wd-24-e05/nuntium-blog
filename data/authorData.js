a283a6347ab792c04e0679d9b2f433e959456177
AlbertLeipzig <albertclemvill@gmail.com>
2024-11-15 10:42:11 +0100

    changed data folder

---
+++ data/authorData.js
@@ -0,0 +1,13 @@
+export const testAuthor = {
+  id: "alksjdfhalsukfhasldkfjhasdff81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+  firstName: "Arthur",
+  lastName: "Black",
+  portrait:
+    "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+  presentation:
+    "Ipsum adipisicing culpa est nisi consequat ex amet magna culpa veniam tempor irure ea. Reprehenderit labore do tempor eiusmod in consectetur ex sunt id mollit commodo ipsum deserunt quis.",
+  website: "arthurblack.com",
+  linkedin: "https://www.linkedin.com/arthurBlack",
+  instagram: "https://www.instagram.com/arthurBlack",
+  twitter: "https://www.twitter.com/arthurBlack",
+};
---
+++ data/cardData.js
@@ -0,0 +1,11 @@
+export const testCard = {
+    id: "alksjdfhalsukfhasldkfjhasdff81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+    image:
+      "https://images.unsplash.com/photo-1731224365250-0cf29169dd32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+    tag: "MAXIMALISM",
+    title: "Culpa sit Laboris Exercitation ea Fugiat",
+    author: "Leslie Pena",
+    date: "April 25, 2012",
+    readTime: "6 mins read",
+    text: "Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit. Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo ullamco labore et esse.",
+  };
